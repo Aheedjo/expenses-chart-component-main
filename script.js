fce09f023de8d78dc7ba94989c6eff9b8135c856
Maishanu Ahidjo <maishanu.ahijo@yahoo.com>
2022-06-07 16:43:49 +0100

    changes

---
+++ script.js
@@ -0,0 +1,41 @@
+// const array = [{
+//         "day": "mon",
+//         "amount": 17.45
+//     },
+//     {
+//         "day": "tue",
+//         "amount": 34.91
+//     },
+//     {
+//         "day": "wed",
+//         "amount": 52.36
+//     },
+//     {
+//         "day": "thu",
+//         "amount": 31.07
+//     },
+//     {
+//         "day": "fri",
+//         "amount": 23.39
+//     },
+//     {
+//         "day": "sat",
+//         "amount": 43.28
+//     },
+//     {
+//         "day": "sun",
+//         "amount": 25.48
+//     }
+// ]
+
+// const days = document.querySelectorAll(".day")
+// days.forEach((day, index) => {
+//     console.log(day)
+//     array.forEach((item, index) => {
+//         day.textContent = item.day
+//     })
+// })
+
+// // array.forEach((item, index) => {
+// //     console.log(item[index])
+// // })
